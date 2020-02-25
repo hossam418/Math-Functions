27a9ddfe1f55201c25531c948e81d586f3b10075
Belal Hamdy <36121094+belalhamdy@users.noreply.github.com>
2020-02-25 12:44:54 +0200

    Skeleton Code

---
+++ src/com/company/SimpleSubscriber.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class SimpleSubscriber implements ISubscriber{
+    @Override
+    public void notifySubscriber(String input) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello, I am a simple subscriber and I am notified with " + input);
+    }
+}
