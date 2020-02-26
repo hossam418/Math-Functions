1dca2d951060362967c19be06a1d844ed19104e5
Belal Hamdy <36121094+belalhamdy@users.noreply.github.com>
2020-02-27 01:59:51 +0200

    Added Lucas Series Formula

--- src/com/company/SimpleSubscriber.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class SimpleSubscriber implements ISubscriber{
-    @Override
-    public void notifySubscriber(String input) {
-        System.out.println("Hello, I am a simple subscriber and I am notified with " + input);
-    }
-}
