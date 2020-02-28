91ee24bbdd764c48b5c8a8a90f465cebac9df4b7
nooratef <36491699+nooratef@users.noreply.github.com>
2020-02-28 19:04:59 +0200

    Sphere Area

---
+++ src/com/company/SphereAreaSubscriber.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class SphereAreaSubscriber implements ISubscriber {
+    static float pi = 3.14159f;
+    @Override
+    public void notifySubscriber(String input) {
+
+          double Radius = Double.valueOf(input);
+            System.out.println( "Sphere Area = "+Calculate_Area(Radius));
+}
+double Calculate_Area(double radius){
+        return (4 * pi * radius * radius);
+}
+
+}
